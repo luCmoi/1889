5569adbcf372a7beb3b93efbcb12a7921b4d40a3
luCmoi <lucmoua@gmail.com>
2016-10-26 16:28:18 +0200

    LockMaj
    Lock integrée non finie (pas de résolution)
    Ne pas faire attention aux images :)

--- Multi/Unity/DemoTest/Assets/Script/Object/Puzzle/Lock/LockInteract.cs
+++ Multi/Unity/DemoTest/Assets/Script/Object/Puzzle/Lock/LockInteract.cs
@@ -3,33 +3,63 @@ using System.Collections;
 
 public class LockInteract : MonoBehaviour {
     public bool movement;
+    public bool active;
     public Transform destination;
     public float speedMovement;
+    public RoueLock[] roueLocks;
+    public Vector3 origine;
 
 	// Use this for initialization
 	void Start () {
-	
+        origine =transform.position;
 	}
 
     void Update()
     {
+        if (active && Controller1.Instance.block && Controller1.Instance.inPuzzle)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(1))
+                {
+                movement = true;
+                }
+            }
         if (movement)
         {
-            transform.position = Vector3.MoveTowards(transform.position, destination.position, speedMovement);
-            if (transform.position == destination.position)
+            if (!active)
             {
-                movement = false;
-                Controller1.Instance.block = false;
+                transform.position = Vector3.MoveTowards(transform.position, destination.position, speedMovement);
+                if (transform.position == destination.position)
+                {
+                    movement = false;
+                    active = true;
+                    foreach (RoueLock roue in roueLocks)
+                    {
+                        roue.Activate();
+                    }
+                }
+            }else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, origine, speedMovement);
+                if (transform.position == origine)
+                {
+                    movement = false;
+                    active = false;
+                    foreach (RoueLock roue in roueLocks)
+                    {
+                        roue.Desactivate();
+                        Controller1.Instance.block = false;
+                    }
+                }
             }
         }
     }
 
         void OnMouseDown()
-    {
-        if (Controller1.Instance.inPuzzle && !Controller1.Instance.block && !Controller1.Instance.inZoomItem)
         {
-            movement = true;
-            Controller1.Instance.block = false;
-        }
+            if (Controller1.Instance.inPuzzle && !Controller1.Instance.block && !Controller1.Instance.inZoomItem)
+            {
+                movement = true;
+                Controller1.Instance.block = true;
+            } 
         }
 }
---
+++ Multi/Unity/DemoTest/Assets/Script/Object/Puzzle/Lock/RoueLock.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoueLock : MonoBehaviour {
+    public float xSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Activate()
+    {
+        GetComponent<MeshCollider>().enabled = true;
+    }
+
+    public void Desactivate()
+    {
+        GetComponent<MeshCollider>().enabled = false;
+    }
+
+    void OnMouseDrag()
+    {
+        float x = Input.GetAxis("Mouse X") * xSpeed;
+        //float y = Input.GetAxis("Mouse Y") * ySpeed;
+        transform.RotateAround(GetComponent<MeshRenderer>().bounds.center, Vector3.down, x);
+        //transform.RotateAround(GetComponent<MeshRenderer>().bounds.center, Vector3.left, y);
+    }
+}
