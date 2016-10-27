cbc095f032afbc30dfb9f04a8ad1e83d5f094925
SaphirTPW <nath.bonkena@gmail.com>
2016-10-27 16:00:43 +0200

    j'ai rajouté mon script pour les boîtes de dialogues faites par Laura (je me met à jour sur Unity)

---
+++ Multi/Unity/DemoTest/Assets/Script/Nathan - Script/ImageSlideShow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ImageSlideShow : MonoBehaviour {
+
+	public Sprite[] slides;
+	public Sprite displayImage;
+	private float changeTime = 10.0f;
+	private int currentSlide = 0;
+	private float timeSinceLast = 1.0f;
+    
+    
+	public void NextImage()
+	{
+		if (timeSinceLast > changeTime && currentSlide < slides.Length) 
+		{
+			currentSlide++;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		displayImage.texture = slides[currentSlide];
+	}
+}
